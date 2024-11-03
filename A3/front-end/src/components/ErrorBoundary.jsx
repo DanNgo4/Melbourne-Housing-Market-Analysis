90e99c0ce53ccbfc68bbaa8a7a3c795300854736
MitchellHenry <48079181+MitchellHenry@users.noreply.github.com>
2024-11-03 18:41:13 +1100

    error boundary comments

--- A3/front-end/src/components/ErrorBoundary.jsx
+++ A3/front-end/src/components/ErrorBoundary.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 
+//This is the ErrorBoundary Class to catch errors in a react components lifecycle
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
     this.state = { hasError: false };
   }
 
+  //update hasError to true
   static getDerivedStateFromError(error) {
     return { hasError: true };
   }
 
+  //Log after an error is thrown
   componentDidCatch(error, errorInfo) {
     console.log(error, errorInfo);
   }
 
+  //Check if it has an error else display the component
   render() {
     if (this.state.hasError) {
       return <h1>Something went wrong.</h1>;
